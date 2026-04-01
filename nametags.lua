d8f47b82d50c08215de4e5e281c544e88d60717c
ykknzo-hub <kenzielimonn@gmail.com>
2026-04-01 23:17:00 +0100

    Update nametags via KIRO bot

--- nametags.lua
+++ nametags.lua
@@ -34,7 +34,13 @@ local LOGO_ASSET_ID = "rbxassetid://121655015965144"
 local TAG_CORNER    = UDim.new(0, 10)
 
 local customPlayers = {
-
+	["dfdf"] = {
+		color      = Color3.fromRGB(0,0,0),
+		glowColor  = Color3.fromRGB(0,0,0),
+		customName = "dfdfdf",
+		gradientA  = Color3.fromRGB(255,255,255),
+		gradientB  = Color3.fromRGB(0,0,0),
+	},
 }
 
 starterGui:SetCore("SendNotification", {
