d5b49892bbdb03d514e38a124b32c8494afd435f
ykknzo-hub <kenzielimonn@gmail.com>
2026-04-06 00:14:50 +0100

    Update nametags via KIRO bot

--- nametags.lua
+++ nametags.lua
@@ -72,7 +72,13 @@ local customPlayers = {
 		customName = "KIRO STAFF",
 		gradientA  = Color3.fromRGB(128,0,128),
 		gradientB  = Color3.fromRGB(128,0,128),
-		logoAsset  = "rbxassetid://121655015965144",
+	},
+	["Adamalchoum2"] = {
+		color      = Color3.fromRGB(128,0,128),
+		glowColor  = Color3.fromRGB(128,0,128),
+		customName = "KIRO STAFF",
+		gradientA  = Color3.fromRGB(128,0,128),
+		gradientB  = Color3.fromRGB(128,0,128),
 	},
 }
 
